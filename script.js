5687a87ec74f0dbae51cc05a6f5cd38d7da9d6dd
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-01-07 13:07:20 +0100

    display input form functionality

--- script.js
+++ script.js
@@ -3,44 +3,50 @@
 //elements
 const form = document.querySelector('.form')
 const inputDistance = document.querySelector('#distance')
+const inputDuration = document.querySelector('#duration')
+const inputElevation = document.querySelector('#elevation')
+
+let map, mapEvent
 
 if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(function(position){    
         const {latitude, longitude} = position.coords
         const locationCord = [latitude, longitude]
-        var map = L.map('map').setView(locationCord, 13);
+        map = L.map('map').setView(locationCord, 13);
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+        }).addTo(map);
 
-        L.marker(locationCord).addTo(map)
-        .bindPopup('Activity')
-        .openPopup();
+        // L.marker(locationCord).addTo(map)
+        // .bindPopup('Activity')
+        // .openPopup();
 
-        map.on('click', function(e){
-            console.log(e)
-            const {lat, lng} = e.latlng
+        map.on('click', function(mapE){
+            mapEvent = mapE
             form.classList.remove('hidden')
             inputDistance.focus()
-            L.marker([lat,lng]).addTo(map)
-            .bindPopup(
-                L.popup({
-                    maxWidth: 250,
-                    minWidth: 100,
-                    autoClose: false,
-                    closeOnClick: false,
-                    className: 'card__border-running'
-                })
-            )
-            .setPopupContent('workout')
-            .openPopup();
         })
-        },function(){
+        },
+        function(){
         alert('you can not access geolocation')
     })
 }
 
-form.addEventListener('sumbit',()=>{
-    
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+    inputDistance.value = inputDuration.value = inputElevation.value = 0
+    const {lat,lng} = mapEvent.latlng
+    L.marker([lat,lng]).addTo(map)
+    .bindPopup(
+        L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'card__border-running'
+        })
+    )
+    .setPopupContent('workout')
+    .openPopup();
 })
