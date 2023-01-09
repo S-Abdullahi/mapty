7bf1cdfcbe984acdfe3b8ba1479c869230c9ccd5
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-01-09 14:33:45 +0100

    creating working

--- script.js
+++ script.js
@@ -8,7 +8,42 @@ const inputDuration = document.querySelector('#duration')
 const inputElevation = document.querySelector('#elevation')
 const inputCadence = document.querySelector('#cadence')
 
-let map, mapEvent
+class Workout{
+    date = new Date()
+    id = (Date.now() + '').slice(-10)
+    constructor(cords, duration, distance){
+       this.cords = cords
+       this.duration = duration
+       this.distance = distance
+    }
+}
+
+class Cycling extends Workout{
+    constructor(cords, duration, distance,elevation){
+        super(cords, duration, distance)
+        this.elevation = elevation
+        this.calcSpeed
+    }
+
+    calcSpeed(){
+        this.speed = this.distance / (this.duration/60)
+        return this.speed
+    }
+}
+
+class Running extends Workout{
+    constructor(cords, duration, distance, cadence){
+        super(cords, duration, distance)
+        this.cadence = cadence
+        this.calcPace
+    }
+
+    calcPace(){
+        this.pace = this.duration / this.distance
+        return this.pace
+    }
+}
+
 
 class App {
     #map
